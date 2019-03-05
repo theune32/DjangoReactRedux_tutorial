ba2c7861e9a5c9bca6706cbefcdbc5681f1c6359
Wouter Theune <>
2019-03-05 20:23:40 +0100

    step 2 finished

--- frontend/src/components/PonyNote.jsx
+++ frontend/src/components/PonyNote.jsx
@@ -1,29 +1,67 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
+import {notes} from "../actions";
+
 
 class PonyNote extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Welcome to PonyNote!</h2>
-        <hr />
 
-        <h3>Notes</h3>
-        <table>
-          <tbody>
-            {this.props.notes.map(note => (
-              <tr>
-                <td>{note.text}</td>
-                <td><button>edit</button></td>
-                <td><button>delete</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+    state = {
+      text: "",
+      updateNoteId: null,
+    }
+
+    resetForm = () => {
+      this.setState({text: "", updateNoteId: null});
+    }
+
+    selectForEdit = (id) => {
+      let note = this.props.notes[id];
+      this.setState({text: note.text, updateNoteId: id});
+    }
+
+    submitNote = (e) => {
+      e.preventDefault();
+      if (this.state.updateNoteId === null) {
+        this.props.addNote(this.state.text);
+      } else {
+        this.props.updateNote(this.state.updateNoteId, this.state.text);
+      }
+      this.resetForm();
+    }
+
+    render() {
+        return (
+          <div>
+            <h2>Welcome to PonyNote!</h2>
+            <hr />
+
+              <h3>Add new note</h3>
+                <form onSubmit={this.submitNote}>
+                  <input
+                    value={this.state.text}
+                    placeholder="Enter note here..."
+                    onChange={(e) => this.setState({text: e.target.value})}
+                    required />
+                    <button onClick={this.resetForm}>Reset</button>
+                  <input type="submit" value="Save Note" />
+                </form>
+
+              <h3>Notes</h3>
+              <table>
+                <tbody>
+                    {this.props.notes.map((note, id) => (
+                      <tr key={`note_${id}`}>
+                        <td>{note.text}</td>
+                        <td><button onClick={() => this.selectForEdit(id)}>edit</button></td>
+                        <td><button onClick={() => this.props.deleteNote(id)}>delete</button></td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+          </div>
+        )
+    }
 }
 
 
@@ -33,8 +71,19 @@ const mapStateToProps = state => {
   }
 }
 
+
+
 const mapDispatchToProps = dispatch => {
   return {
+    addNote: (text) => {
+      dispatch(notes.addNote(text));
+    },
+    updateNote: (id, text) => {
+      dispatch(notes.addNote(id, text));
+    },
+    deleteNote: (id) => {
+      dispatch(notes.deleteNote(id));
+    },
   }
 }
 
