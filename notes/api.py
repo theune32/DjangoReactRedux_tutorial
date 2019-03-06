4bafba21afa890380c14a980897f97305e367f89
Wouter Theune <>
2019-03-06 18:20:36 +0100

    added user registration/login

--- notes/api.py
+++ notes/api.py
@@ -1,10 +1,38 @@
-from rest_framework import viewsets, permissions
+from knox.models import AuthToken
+from rest_framework import viewsets, permissions, generics
+from rest_framework.response import Response
 
 from .models import Note
-from .serializers import NoteSerializer
+from .serializers import NoteSerializer, CreateUserSerializer, UserSerializer, LoginUserSerializer
 
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     permission_classes = [permissions.AllowAny, ]
     serializer_class = NoteSerializer
+
+
+class RegistrationAPI(generics.GenericAPIView):
+    serializer_class = CreateUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)
+        })
+
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)
+        })
--- notes/endpoints.py
+++ notes/endpoints.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .api import NoteViewSet
+from .api import NoteViewSet, RegistrationAPI, LoginAPI
 
 router = routers.DefaultRouter()
 router.register('notes', NoteViewSet)
 
 urlpatterns = [
     url("^", include(router.urls)),
+    url("^auth/register/$", RegistrationAPI.as_view()),
+    url("^auth/login/$", LoginAPI.as_view()),
 ]
